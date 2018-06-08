4db717b034c07f6669f69da7375b9e339b3023e8
dooley.nsewolo <nsewolo@gmail.com>
2018-06-08 10:23:33 -0400

    Renaming of index to starter

--- src/interfaces/http/bicycle-weather-rest-api.js
+++ src/interfaces/http/bicycle-weather-rest-api.js
@@ -1,22 +1,23 @@
 import express from 'express';
-import bodyParser from "body-parser";
+import bodyParser from 'body-parser';
 
 const HTTP_PORT = 3000;
 const app = express();
 const router = express.Router();
 
 export class BicycleWeatherRestApi {
-
-    starter() {
+    constructor(){
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({extended: true}));
+    }
 
-        app.get('/', function (req, res) {
+    starter() {
+        app.get('/', (req, res) => {
             console.log('Calling path: ', req.route.path);
             res.send('hello world')
         });
 
-        app.get('/health', function (req, res) {
+        app.get('/health', (req, res) => {
             console.log('Calling path: ', req.route.path);
             res.send('Info');
         });
